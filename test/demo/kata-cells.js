c0cca72d93071b5d644592a7b42488e1a32432c7
pc2 <dominuskernel@gmail.com>
2017-07-26 16:48:10 +0200

    property point fail

---
+++ test/demo/kata-cells.js
@@ -0,0 +1,6 @@
+'use strict';
+module.exports = class Game {
+  construct(){
+    this.points = [[14,20], [18,40], [50,80], [42,50]];
+  }
+}
--- test/kata1.js
+++ test/kata1.js
@@ -1,6 +1,8 @@
+const Game = require('./demo/kata-cells.js');
+
 describe("kata", function() {
   it("exist the four points", function() {
-    const points = [[14,20], [18,40], [50,80], [42,50]]
-    expect(points.length).toBe(4);
+    const game = new Game();
+    expect(game.points).toBe(false);
   });
 });
