4aea28b75f08f50ac7869f3fd32b47d574594b86
pc2 <dominuskernel@gmail.com>
2017-07-26 16:21:12 +0200

    test fail

--- test/kata1.js
+++ test/kata1.js
@@ -1,6 +1,6 @@
 describe("kata", function() {
   it("exist the four points", function() {
-    const points = [[14,20], [18,40], [50,80], [42,50]]
+    const points = [[14,20], [18,40], [50,80]]
     expect(points.length).toBe(4);
   });
 });
