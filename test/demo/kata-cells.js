c071c2447c4906c9f1224637521dd5d9a2b3f717
pc2 <dominuskernel@gmail.com>
2017-07-26 17:26:17 +0200

    first test

--- test/demo/kata-cells.js
+++ test/demo/kata-cells.js
@@ -1,11 +1,14 @@
 'use strict';
 
-module.exports = class Game {
-  constructor(){
-    this.points = [[14,20], [18,40], [50,80], [42,50]];
+let points = [[14,20], [18,40], [50,80], [42,50]];
 
+class Game {
+  constructor(){
   }
-  getPoints(){
-  	return this.points;
+
+  getPoints() {
+    return points;
   }
 }
+
+module.exports = Game;
--- test/kata1.js
+++ test/kata1.js
@@ -2,7 +2,7 @@ let Game = require('./demo/kata-cells.js');
 
 describe("kata", function() {
   it("exist the four points", function() {
-    var game = new Game();
-    expect(game.getPoints()).toBe(false);
+    const game = new Game();
+    expect(game.getPoints().length).toBe(4);
   });
 });
