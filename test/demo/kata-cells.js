26cb0d257b4b930c88140c0886faa615c70c4d1f
nati08 <natcatnote@gmail.com>
2017-07-26 12:16:16 -0300

    class game

--- test/demo/kata-cells.js
+++ test/demo/kata-cells.js
@@ -1,6 +1,11 @@
 'use strict';
+
 module.exports = class Game {
-  construct(){
+  constructor(){
     this.points = [[14,20], [18,40], [50,80], [42,50]];
+
+  }
+  getPoints(){
+  	return this.points;
   }
 }
--- test/kata1.js
+++ test/kata1.js
@@ -1,8 +1,8 @@
-const Game = require('./demo/kata-cells.js');
+let Game = require('./demo/kata-cells.js');
 
 describe("kata", function() {
   it("exist the four points", function() {
-    const game = new Game();
-    expect(game.points).toBe(false);
+    var game = new Game();
+    expect(game.getPoints()).toBe(false);
   });
 });
